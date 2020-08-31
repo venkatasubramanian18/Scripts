aa946ea9e24d4ab0a6953feacc61153449360033
devopsbc01 <70231154+devopsbc01@users.noreply.github.com>
2020-08-30 21:50:39 -0700

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,9 +1,12 @@
 #bash to install tomcat
+# $logfile is the log file for this installation.
+logfile='tomcatlinstall-log'
 #----------------------
-sudo apt-get update
-sudo apt-get install default-jdk
-sudo apt-get install tomcat8
-sudo apt-get install tomcat8-docs tomcat8-examples tomcat8-admin
+sudo su -
+sudo apt-get update >> $logfile
+sudo apt-get install default-jdk >> $logfile
+sudo apt-get install tomcat8 >> $logfile
+sudo apt-get install tomcat8-docs tomcat8-examples tomcat8-admin >> $logfile
 #bash to admin-user in tomcat
 #----------------------------
 sed -i 's+</tomcat-users>+  <role rolename="admin-script"/>+gI' /var/lib/tomcat8/conf/tomcat-users.xml
@@ -16,6 +19,6 @@ echo '  <user username="tomcat" password="tomcat" roles="admin, manager,admin-sc
 echo "</tomcat-users>"  >> /var/lib/tomcat8/conf/tomcat-users.xml
 #bash to restart tomcat
 #----------------------
-sudo systemctl restart tomcat8
+sudo systemctl restart tomcat8 >> $logfile
 sleep 5
 sudo systemctl status tomcat8
