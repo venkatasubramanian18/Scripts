0f06fe6c73af030b312a7610bca7df3e3ab9f7fc
devopsbc01 <70231154+devopsbc01@users.noreply.github.com>
2020-09-01 07:15:42 -0700

    Create sonar.sh

---
+++ sonar.sh
@@ -0,0 +1,9 @@
+sudo apt update
+wget https://raw.githubusercontent.com/devopsbc01/Scripts/master/docker-compose.yml
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+sudo sysctl -w vm.max_map_count=262144
+docker-compose up -d
+docker ps
+docker-compose logs -f sonarqube
