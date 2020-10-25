21b89b596838d1ad9dec0b4583b34c1f8114c88d
venkatasubramanian18 <venkatasubramanian18@gmail.com>
2020-10-25 13:35:13 -0700

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -1,10 +1,10 @@
 sudo apt update
 cd jenkins
-wget https://raw.githubusercontent.com/venkatasubramanian18/Scripts/master/docker-compose.yml
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+#wget https://raw.githubusercontent.com/venkatasubramanian18/Scripts/master/docker-compose.yml
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+#docker-compose --version
 sudo sysctl -w vm.max_map_count=262144
 docker-compose up -d
 docker ps
-docker-compose logs -f sonarqube
+#docker-compose logs -f sonarqube
